9246d9114918de394107b5fe95a51a6df0b38739
Nathan Nicholson <nathan.bytecode@gmail.com>
2023-11-08 14:05:33 +0000

    Update debug to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nathanbyteco-djangoblog-xsrr51jzb45.ws-eu106.gitpod.io', 'https://codestar356-d6b025e33941.herokuapp.com/', 'localhost']
 
